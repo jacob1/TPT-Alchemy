1c7429b0ba5c5aaa94adad951b0765d60047a2b7
SopaXorzTaker <markboldyrv@gmail.com>
2016-07-03 12:55:58 +0300

    Acheivements

--- src/gui/game/Acheivement.cpp
+++ src/gui/game/Acheivement.cpp
@@ -1,1 +1,82 @@
-// TODO add acheivements
+#include "simulation/Simulation.h"
+#include "gui/game/Acheivement.h"
+
+bool Acheivement::checkCompletion(Simulation * sim) {
+	for (std::vector<int>::iterator it = requiredElements.begin(); it < requiredElements.end(); it++)
+		if (!sim->elementsAcquired[*it])
+			return false;
+	return true;
+};
+
+BasicAcheivement::BasicAcheivement() {
+	title = "First steps!";
+	text = "Congratulations, you made the first elements!";
+};
+
+DiamondAcheivement::DiamondAcheivement() {
+	requiredElements.push_back(PT_DMND);
+	requiredElements.push_back(PT_GOLD);
+        title = "Gettin' rich";
+        text = "Wow, you are now rich. Joke. But you made DMND and GOLD!";
+};
+
+ElectronicsAcheivement::ElectronicsAcheivement() {
+	// TODO requiredElements
+        title = "Beep-bloop!";
+        text = "You have acquired all the electronic elements! 1010 10110101!";
+};
+
+ExplosivesAcheivement::ExplosivesAcheivement() {
+        // TODO requiredElements
+        title = "Kaboom!";
+        text = "You have gathered all the explosive elements!";
+};
+
+
+NuclearAcheivement::NuclearAcheivement() {
+	// TODO requiredElements
+        title = "Science!";
+        text = "You found all the nuclear elements. But please be careful though :)";
+};
+
+ExotAcheivement::ExotAcheivement() {
+	requiredElements.push_back(PT_EXOT);
+        title = "Yotredwop!";
+        text = "EXOT is sure pretty weird";
+};
+
+StickmanAcheivement::StickmanAcheivement() {
+	requiredElements.push_back(PT_STKM);
+	requiredElements.push_back(PT_STKM2);
+	requiredElements.push_back(PT_FIGH);
+        title = "Hey!";
+        text = "You got all the Stickmen!";
+};
+
+PortalAcheivement::PortalAcheivement() {
+	requiredElements.push_back(PT_PRTI);
+	requiredElements.push_back(PT_PRTO);
+        title = "Woosh!";
+        text = "You have portals now.";
+};
+
+ExtraAcheivement::ExtraAcheivement() {
+	requiredElements.push_back(PT_116);
+	requiredElements.push_back(PT_LOVE);
+	requiredElements.push_back(PT_LOLZ);
+	requiredElements.push_back(PT_MORT);
+        title = "WAIT, HOW?!";
+        text = "You found a way to make all the extra elements! How?!";
+};
+
+AllAcheivement::AllAcheivement() {
+	// TODO requiredElements
+        title = "Congratulations!";
+        text = "You have managed to get all the elements available!\n"
+        "Hereby, we note that you are officially considered to have completed this game.\n"
+        "Good luck and best wishes! Have fun!\n"
+        "\n"
+        "- \bosavask\bg, \boSopaXorzTaker\bg, \boand others\bg.";
+};
+
+
--- src/gui/game/Acheivement.h
+++ src/gui/game/Acheivement.h
@@ -3,77 +3,43 @@
 
 #include "simulation/Simulation.h"
 
-#define ACHEIVEMENT_BASIC 1
-#define ACHEIVEMENT_DIAMOND 2
-#define ACHEIVEMENT_ELECTRONICS 3
-#define ACHEIVEMENT_EXPLOSIVES 4
-#define ACHEIVEMENT_NUCLEAR 5
-#define ACHEIVEMENT_EXOT 6
-#define ACHEIVEMENT_PORTAL 7
+#define ACHEIVEMENT_BASIC 0
+#define ACHEIVEMENT_DIAMOND 1
+#define ACHEIVEMENT_ELECTRONICS 2
+#define ACHEIVEMENT_EXPLOSIVES 3
+#define ACHEIVEMENT_NUCLEAR 4
+#define ACHEIVEMENT_EXOT 5
+#define ACHEIVEMENT_PORTAL 6
+#define ACHEIVEMENT_STICKMAN 7
 #define ACHEIVEMENT_EXTRA 8
 #define ACHEIVEMENT_ALL 9
 
+#define ACHEIVEMENT_COUNT 10
+
 using namespace std;
-
-// TODO - make stuff object oriented
-
-const char* achievementBasicTitle = "First steps!";
-const char* achievementBasicText = "Congratulations, you made the first elements!";
-
-const char* achievementElectronicsTitle = "Beep-bloop!";
-const char* achievementElectronicsText = "You have acquired all the electronic elements! 1010 10110101!";
-
-const char* achievementDiamondTitle = "Gettin' rich";
-const char* achievementDiamondText = "Wow, you are now rich. Joke. But you made DMND!";
-
-const char* achievementExplosivesTitle = "Kaboom!";
-const char* achievementExplosivesText = "You have gathered all the explosive elements!";
-
-const char* achievementNuclearTitle = "Science!";
-const char* achievementNuclearText = "You found all the nuclear elements. But please be careful though :)";
-
-const char* achievementExotTitle = "Yotredwop!";
-const char* achievementExotText = "EXOT is sure pretty weird";
-
-const char* achievementPortalTitle = "Woosh!";
-const char* achievementPortalText = "You have portals now.";
-
-const char* achievementExtraTitle = "WAIT, HOW?!";
-const char* achievementExtraText = "You found a way to make all the extra elements! How?!";
-
-const char* achievementAllTitle = "Congratulations!";
-const char* achievementAllText = "You have managed to get all the elements available!\n"
-	"Hereby, we note that you have officially considered to have completed this game.\n"
-	"Good luck and best wishes! Have fun!\n"
-	"\n"
-	"- \bosavask\bg, \boSopaXorzTaker\bg, \boand others\bg.";
-
-//TODO
-
-int basicElements[] = {
-
-};
-
-int extraElements[] = {
-
-};
-
-int allElements[] = {
-
-};
 
 class Acheivement {
 	public:
 		Acheivement();
 		~Acheivement();
-		std::string GetTitle();
-		std::string GetText();
+
 		bool checkCompletion(Simulation * sim);
-	private:
+
 		std::string title;
 		std::string text;
+		std::vector<int> requiredElements;
 };
 
-// TODO add acheivements
+class BasicAcheivement: public Acheivement {public: BasicAcheivement();};
+class DiamondAcheivement: public Acheivement {public: DiamondAcheivement();};
+class ElectronicsAcheivement: public Acheivement {public: ElectronicsAcheivement();};
+class ExplosivesAcheivement: public Acheivement {public: ExplosivesAcheivement();};
+class NuclearAcheivement: public Acheivement {public: NuclearAcheivement();};
+class ExotAcheivement: public Acheivement {public: ExotAcheivement();};
+class PortalAcheivement: public Acheivement {public: PortalAcheivement();};
+class StickmanAcheivement: public Acheivement {public: StickmanAcheivement();};
+class ExtraAcheivement: public Acheivement {public: ExtraAcheivement();};
+class AllAcheivement: public Acheivement {public: AllAcheivement();};
+
 
 #endif
